b21e03f322a6648ee72decf14bebbd92b338d06e
Maratib Ali Khan <maratib@gmail.com>
2022-05-18 20:52:20 +0500

    postcss added

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss",
   ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  }
-}
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+};
