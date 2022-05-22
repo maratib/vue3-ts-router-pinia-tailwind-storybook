9eacdf7a287080c790597c2a7d45e0350e129f98
Maratib Ali Khan <maratib@gmail.com>
2022-05-23 00:09:13 +0500

    adsf

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const { label, bgColor } = withDefaults(
   defineProps<{ label: string; bgColor?: string }>(),
